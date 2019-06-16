13cf84f3fc45b97979f7603d43ca935cd968ba3c
Andrew Coven <a@cov.in>
2019-06-16 16:52:44 -0700

    Remove garbage can from GtT icon by removing that class (Bq); make popup location below icon instead of on top of it.

--- chrome/views/popupView.js
+++ chrome/views/popupView.js
@@ -89,7 +89,7 @@ GmailToTrello.PopupView.prototype.confirmPopup = function() {
             this.html['add_to_trello'] =
                 '<div id="gttButton" class="T-I J-J5-Ji ar7 nf T-I-ax7 L3" ' // "lS T-I-ax7 ar7"
                   + 'data-tooltip="Add this Gmail to Trello">'
-                  + '<div aria-haspopup="true" role="button" class="J-J5-Ji W6eDmd L3 J-J5-Ji Bq L3" tabindex="0">' // class="J-J5-Ji W6eDmd L3 J-J5-Ji Bq L3">' //
+                  + '<div aria-haspopup="true" role="button" class="J-J5-Ji W6eDmd L3 J-J5-Ji L3" tabindex="0">' // class="J-J5-Ji W6eDmd L3 J-J5-Ji Bq L3">' // Bq = Delete icon
                   + img
                   + '<div id="gttDownArrow" class="G-asx T-I-J3 J-J5-Ji">&nbsp;</div></div></div>';
         }
